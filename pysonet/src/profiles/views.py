bfb8802824ce3ff16e754b253932d9c5d9f07737
OlegVostokUA <93160512+OlegVostokUA@users.noreply.github.com>
2022-02-13 23:38:35 +0200

    13.02

--- pysonet/src/profiles/views.py
+++ pysonet/src/profiles/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.generics import RetrieveAPIView
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import UserNet
+from .serializers import GetUserNetSerializer
+
+
+class GetUserNetView(RetrieveAPIView):
+    queryset = UserNet.objects.all()
+    serializer_class = GetUserNetSerializer
+
+
+"""
+class UserNetPublicView(ModelViewSet):
+   
+    queryset = UserNet.objects.all()
+    serializer_class = GetUserNetPublicSerializer
+    permission_classes = [permissions.AllowAny]
+
+"""
